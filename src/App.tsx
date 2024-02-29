39cc35029a51118a85b7ba4616e114fbdeca9306
Emujede Hilary <104493382+dev-hills@users.noreply.github.com>
2024-02-29 16:48:55 +0100

    Fixed route issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./AppRoutes";
+import { useEffect } from "react";
 
 const App = () => {
+  const currentLocation = window.location.pathname;
   const reactQueryClient = new QueryClient({
     defaultOptions: {
       queries: {
         retry: false,
       },
     },
+  });
+  useEffect(() => {
+    if (currentLocation !== "/login") {
+      window.location.href = "/login";
+    }
   });
   return (
     <div>
