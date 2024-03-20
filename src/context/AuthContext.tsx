e4022274cbb8ca3711ef9a7145f52e2dc5b7ee69
Emujede Hilary <104493382+dev-hills@users.noreply.github.com>
2024-03-20 15:02:07 +0100

    Reverted to former auth process again

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { typeAuthContext } from "../utils/types";
 
 const AuthContext = createContext<typeAuthContext | undefined>(undefined);
